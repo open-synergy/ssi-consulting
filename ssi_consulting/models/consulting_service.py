63a8bd76ee5b81bdabc26489fe572bf8b7e4bdfb
Andhitia Rama <andhitia.r@gmail.com>
2025-09-06 22:47:02 +0700

    [UPD] ssi_consulting
    * Menghilangkan ai_prompt
    * Menambahkan default system dan user prompting pada report template

--- ssi_consulting/models/consulting_service.py
+++ ssi_consulting/models/consulting_service.py
@@ -445,6 +445,46 @@ class ConsultingService(models.Model):
     def onchange_report_template_id(self):
         self.report_template_id = False
 
+    @api.onchange(
+        "report_template_id",
+    )
+    def onchange_system_prompting_schema_parser_id(self):
+        self.system_prompting_schema_parser_id = False
+        if self.report_template_id:
+            self.system_prompting_schema_parser_id = (
+                self.report_template_id.system_prompting_schema_parser_id
+            )
+
+    @api.onchange(
+        "report_template_id",
+    )
+    def onchange_system_prompting_specification(self):
+        self.system_prompting_specification = ""
+        if self.report_template_id:
+            self.system_prompting_specification = (
+                self.report_template_id.system_prompting_specification
+            )
+
+    @api.onchange(
+        "report_template_id",
+    )
+    def onchange_user_prompting_schema_parser_id(self):
+        self.user_prompting_schema_parser_id = False
+        if self.report_template_id:
+            self.user_prompting_schema_parser_id = (
+                self.report_template_id.user_prompting_schema_parser_id
+            )
+
+    @api.onchange(
+        "report_template_id",
+    )
+    def onchange_user_prompting_specification(self):
+        self.user_prompting_specification = ""
+        if self.report_template_id:
+            self.user_prompting_specification = (
+                self.report_template_id.user_prompting_specification
+            )
+
     # ===========================
     # Utilities: newline & comment handling
     # ===========================
